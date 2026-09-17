e52d4b19fb216b55430aabb82ae842c7c5561cae
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 16:33:12 -0500

    ejercicio29

---
+++ src/ejercicio29/Persona.java
@@ -0,0 +1,22 @@
+package ejercicio29;
+
+class Persona {
+    String nombre;
+    int edad;
+}
+
+public class Empleado extends Persona {
+    double salario;
+
+    void mostrarInfoEmpleado() {
+        System.out.println("Empleado: " + nombre + " | Edad: " + edad + " | Salario: $" + salario);
+    }
+
+    public static void main(String[] args) {
+        Empleado emp = new Empleado();
+        emp.nombre = "Carlos";
+        emp.edad = 30;
+        emp.salario = 2500000;
+        emp.mostrarInfoEmpleado();
+    }
+}
