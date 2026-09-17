af1c5f1439f525ce7af66351725c3da500a77c68
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 14:33:19 -0500

    Ejercicio09

---
+++ src/Ejercicio09/Producto.java
@@ -0,0 +1,22 @@
+package Ejercicio09;
+
+public class Producto {
+    String nombre;
+    double precio;
+    int stock;
+
+    void mostrarDetalles() {
+        System.out.println("--- Detalles del Producto ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + stock + " unidades");
+    }
+
+    public static void main(String[] args) {
+        Producto prod = new Producto();
+        prod.nombre = "Teclado Mecánico";
+        prod.precio = 150000.0;
+        prod.stock = 15;
+        prod.mostrarDetalles();
+    }
+}
