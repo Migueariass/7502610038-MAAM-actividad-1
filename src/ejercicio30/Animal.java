d9552e214d75c978d8f78ce67c89b4e266930e75
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 16:34:08 -0500

    ejercicio30

---
+++ src/ejercicio30/Animal.java
@@ -0,0 +1,19 @@
+package ejercicio30;
+
+class Animal {
+    void hacerSonido() {
+        System.out.println("El animal hace un sonido genérico.");
+    }
+}
+
+public class Perro extends Animal {
+    @Override
+    void hacerSonido() {
+        System.out.println("El perro ladra: ¡Guau guau!");
+    }
+
+    public static void main(String[] args) {
+        Perro p = new Perro();
+        p.hacerSonido();
+    }
+}
