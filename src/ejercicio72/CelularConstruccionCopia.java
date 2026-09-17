5d7e6830825cc062364825f8ec2932732287d668
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 17:40:55 -0500

    ejercicio72

---
+++ src/ejercicio72/CelularConstruccionCopia.java
@@ -0,0 +1,29 @@
+package ejercicio73;
+
+public class CelularConstructorCopia {
+    String marca;
+    int bateria;
+
+    public CelularConstructorCopia(String marca, int bateria) {
+        this.marca = marca;
+        this.bateria = bateria;
+    }
+
+    // Constructor copia
+    public CelularConstructorCopia(CelularConstructorCopia otro) {
+        this.marca = otro.marca;
+        this.bateria = otro.bateria;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Celular: " + marca + " | Batería: " + bateria + "%");
+    }
+
+    public static void main(String[] args) {
+        CelularConstructorCopia original = new CelularConstructorCopia("Xiaomi", 85);
+        CelularConstructorCopia copia = new CelularConstructorCopia(original);
+
+        original.mostrarInfo();
+        copia.mostrarInfo();
+    }
+}
