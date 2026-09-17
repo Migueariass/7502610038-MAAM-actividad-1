7a59a2a802e0eca9d0285d2bd4a26dbbbbef26d6
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 16:54:12 -0500

    ejercicio28

--- src/ejercicio28/Calculadora.java
+++ src/ejercicio28/Calculadora.java
@@ -1,12 +1,27 @@
 package ejercicio28;
 
-public class Calculadora {
-    public static double calcularPromedio(double n1, double n2, double n3) {
-        return (n1 + n2 + n3) / 3.0;
+public class CuentaBancaria {
+    String numero;
+    String titular;
+    double saldo;
+
+    public CuentaBancaria(String numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    void mostrarCuenta() {
+        System.out.println("Número: " + numero + " | Titular: " + titular + " | Saldo: $" + saldo);
     }
 
     public static void main(String[] args) {
-        double prom = Calculadora.calcularPromedio(4.0, 3.5, 4.5);
-        System.out.println("El promedio final es: " + prom);
+        CuentaBancaria c1 = new CuentaBancaria("101-202", "Ana Gómez", 150000);
+        CuentaBancaria c2 = new CuentaBancaria("303-404", "Carlos Pérez", 300000);
+        CuentaBancaria c3 = new CuentaBancaria("505-606", "Marta Ríos", 450000);
+
+        c1.mostrarCuenta();
+        c2.mostrarCuenta();
+        c3.mostrarCuenta();
     }
 }
