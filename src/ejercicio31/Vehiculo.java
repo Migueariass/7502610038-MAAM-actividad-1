839e020e395faf624aa2d817126cec16c342618c
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 16:40:26 -0500

    ejercicio31

---
+++ src/ejercicio31/Vehiculo.java
@@ -0,0 +1,27 @@
+package ejercicio31;
+
+class Vehiculo {
+    String marca;
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
+
+public class Coche extends Vehiculo {
+    int numeroPuertas;
+
+    public Coche(String marca, int numeroPuertas) {
+        super(marca);
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    void mostrarDetalles() {
+        System.out.println("Marca: " + marca + " | Puertas: " + numeroPuertas);
+    }
+
+    public static void main(String[] args) {
+        Coche c = new Coche("Chevrolet", 4);
+        c.mostrarDetalles();
+    }
+}
