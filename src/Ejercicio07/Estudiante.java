a1fc7d72bb4712d70bb9b89774595ffe0f554aac
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 14:31:43 -0500

    Ejercicio07

---
+++ src/Ejercicio07/Estudiante.java
@@ -0,0 +1,22 @@
+package ejercicio07;
+
+public class Estudiante {
+    String nombre;
+    String codigo;
+    int semestre;
+
+    void mostrarInfo() {
+        System.out.println("--- Información del Estudiante ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Código: " + codigo);
+        System.out.println("Semestre: " + semestre);
+    }
+
+    public static void main(String[] args) {
+        Estudiante est = new Estudiante();
+        est.nombre = "Miguel Arias";
+        est.codigo = "7502610038";
+        est.semestre = 5;
+        est.mostrarInfo();
+    }
+}
