8f275fb528fe60c8c0a51eb8c8eb200805d4c6be
Miguel Arias <mariasm1@unicartagena.edu.co>
2026-09-17 16:49:36 -0500

    ejercicio38

---
+++ src/ejercicio38/Constantes.java
@@ -0,0 +1,11 @@
+package ejercicio38;
+
+public class Constantes {
+    public static final double PI = 3.14159;
+    public static final String NOMBRE_SISTEMA = "Sistema Ventas";
+
+    public static void main(String[] args) {
+        System.out.println("Sistema: " + NOMBRE_SISTEMA);
+        System.out.println("Valor constante de PI: " + PI);
+    }
+}
